99fb7d41df7fdd2057e2f050589638d3c02413a5
Jerry Crunchtime <jerry.c.t@web.de>
2021-06-04 18:31:19 +0200

    Benchmark clock

--- benchmark.cpp
+++ benchmark.cpp
@@ -34,9 +34,30 @@ void LockFreeQueue(benchmark::State &state) {
   }
 }
 
+void Now(benchmark::State &state) {
+  for (auto _ : state) {
+    const auto now = std::chrono::steady_clock::now();
+    benchmark::DoNotOptimize(now);
+    benchmark::ClobberMemory();
+  }
+}
+void ClockGettime(benchmark::State &state) {
+  for (auto _ : state) {
+    timespec tp;
+    // syscall(SYS_clock_gettime, CLOCK_MONOTONIC, &tp);
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    const auto now = std::chrono::steady_clock::time_point(
+        std::chrono::steady_clock::duration(std::chrono::seconds(tp.tv_sec) + std::chrono::nanoseconds(tp.tv_nsec)));
+    benchmark::DoNotOptimize(now);
+    benchmark::ClobberMemory();
+  }
+}
+
 BENCHMARK(PerThreadEvents);
 BENCHMARK(Span);
 BENCHMARK(LockFreeQueue);
+BENCHMARK(Now);
+BENCHMARK(ClockGettime);
 
 } // namespace
 
