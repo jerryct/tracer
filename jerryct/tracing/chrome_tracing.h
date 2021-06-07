7176fdc72d769fb8c2c9192a38a079480b0b8f0e
Jerry Crunchtime <jerry.c.t@web.de>
2021-06-07 16:16:59 +0200

    Directly print to file without a json lib

--- jerryct/tracing/chrome_tracing.h
+++ jerryct/tracing/chrome_tracing.h
@@ -4,33 +4,53 @@
 #define JERRYCT_TRACING_CHROME_TRACING_H
 
 #include "jerryct/tracing/tracing.h"
-#include "nlohmann/json.hpp"
 #include <chrono>
-#include <fstream>
+#include <cstdio>
 #include <string>
 
 namespace jerryct {
 namespace trace {
 
-struct AsChromeTracingJson {
+class AsChromeTracingJson {
+public:
+  AsChromeTracingJson(const std::string &filename) : f_{fopen(filename.c_str(), "w")} {
+    if (!IsValid()) {
+      throw;
+    }
+    fprintf(f_, "[{}");
+  }
+  AsChromeTracingJson(const AsChromeTracingJson &) = delete;
+  AsChromeTracingJson(AsChromeTracingJson &&other) noexcept : f_{other.f_} { other.f_ = nullptr; }
+  AsChromeTracingJson &operator=(const AsChromeTracingJson &) = delete;
+  AsChromeTracingJson &operator=(AsChromeTracingJson &&other) noexcept {
+    if (f_ != other.f_) {
+      std::swap(f_, other.f_);
+    }
+    return *this;
+  }
+  ~AsChromeTracingJson() noexcept {
+    if (IsValid()) {
+      fprintf(f_, "]");
+      fclose(f_);
+    }
+  }
+  bool IsValid() const { return f_ != nullptr; }
+
   void operator()(const int tid, const jerryct::string_view name, const Phase p,
                   const std::chrono::steady_clock::time_point ts) {
     const auto d = std::chrono::duration<double, std::micro>{ts.time_since_epoch()}.count();
 
-    if (p == Phase::begin)
-      j.push_back({{"name", std::string{name.data(), name.size()}}, {"pid", 0}, {"tid", tid}, {"ph", "B"}, {"ts", d}});
-    if (p == Phase::end)
-      j.push_back({{"pid", 0}, {"tid", tid}, {"ph", "E"}, {"ts", d}});
+    if (p == Phase::begin) {
+      fprintf(f_, R"(,{"name":"%.*s","pid":0,"tid":%d,"ph":"B","ts":%f})", static_cast<int>(name.size()), name.data(),
+              tid, d);
+    }
+    if (p == Phase::end) {
+      fprintf(f_, R"(,{"pid":0,"tid":%d,"ph":"E","ts":%f})", tid, d);
+    }
   }
 
-  AsChromeTracingJson(std::string filename) : j{nlohmann::json::array()}, filename_{filename} {}
-  ~AsChromeTracingJson() {
-    std::ofstream o(filename_);
-    o << j << std::endl;
-  }
-
-  nlohmann::json j;
-  std::string filename_;
+private:
+  std::FILE *f_{nullptr};
 };
 
 } // namespace trace
