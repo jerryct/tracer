346be135793fcaa7d4afe4ec7bb0c849c82fbb87
Jerry Crunchtime <jerry.c.t@web.de>
2021-10-03 18:56:14 +0200

    Collect time

--- jerryct/tracing/tracing.cpp
+++ jerryct/tracing/tracing.cpp
@@ -6,12 +6,12 @@ namespace jerryct {
 namespace trace {
 
 Span::Span(TracerImpl &t, const jerryct::string_view name) : t_{t.PerThreadEvents()} {
-  const auto now = std::chrono::steady_clock::time_point{}; // std::chrono::steady_clock::now();
+  const auto now = std::chrono::steady_clock::now();
   t_->events.Emplace(Phase::begin, now, name);
 }
 
 Span::~Span() noexcept {
-  const auto now = std::chrono::steady_clock::time_point{}; // std::chrono::steady_clock::now();
+  const auto now = std::chrono::steady_clock::now();
   t_->events.Emplace(Phase::end, now, jerryct::string_view{""});
 }
 
